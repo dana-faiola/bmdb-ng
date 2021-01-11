2b1664a8cdfe7abce69a0bca16d1f6d152e3659f
Dana-Kimmel <dear.prudence34@gmail.com>
2021-01-10 21:18:31 -0500

    credit

--- src/app/service/credit.service.ts
+++ src/app/service/credit.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Credit } from '../model/credit.class';
 
+const URL = 'http://localhost:8080/movies';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CreditService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  //service functions
+  // getAll credits
+  getAll(): Observable<Credit[]> {
+    return this.http.get(URL + '/') as Observable<Credit[]>;
+  }
 }
