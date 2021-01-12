9f2c970b9d42f795e92770ce4acd028bea3ef6ed
Dana-Kimmel <dear.prudence34@gmail.com>
2021-01-11 20:05:34 -0500

    actor-create component

---
+++ src/app/feature/actor/actor-create/actor-create.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Actor } from 'src/app/model/actor.class';
+import { ActorService } from 'src/app/service/actor.service';
+
+@Component({
+  selector: 'app-actor-create',
+  templateUrl: './actor-create.component.html',
+  styleUrls: ['./actor-create.component.css']
+})
+export class ActorCreateComponent implements OnInit {
+  title="Actor Create";
+  actor: Actor = new Actor();
+  submitBtnTitle ="Create";
+  constructor(private actorSvc: ActorService) { }
+
+  ngOnInit(): void {
+  }
+  save() {
+    // save the movie to the DB
+    this.actorSvc.create(this.actor).subscribe(
+      resp => {
+        this.actor = resp as Actor;
+        console.log('Actor created',this.actor );
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+}
