82fc120c96e1ea6301ec88728c7b0e61d5a9f4f9
Dana-Kimmel <dear.prudence34@gmail.com>
2021-01-13 21:36:43 -0500

    movie-service

--- src/app/service/movie.service.ts
+++ src/app/service/movie.service.ts
@@ -1,24 +1,38 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Movie } from '../model/movie.class';
 
 const URL = 'http://localhost:8080/movies';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   //service functions
   // getAll movies
-  getAll(): Observable<Movie[]>{
-    return this.http.get(URL+'/') as Observable<Movie[]>;
+  getAll(): Observable<Movie[]> {
+    return this.http.get(URL + '/') as Observable<Movie[]>;
   }
-  
+
+  // get movie by ID
+  getById(id): Observable<Movie> {
+    return this.http.get(URL + '/' + id) as Observable<Movie>;
+  }
+
   // create movie
-  create(movie: Movie): Observable<Movie>{
-    return this.http.post(URL+'/', movie) as Observable<Movie>;
+  create(movie: Movie): Observable<Movie> {
+    return this.http.post(URL + '/', movie) as Observable<Movie>;
+  }
+
+  // update movie
+  update(movie: Movie): Observable<Movie> {
+    return this.http.put(URL + '/', movie) as Observable<Movie>;
+  }
+
+   // delete movie
+   delete(id): Observable<Movie> {
+    return this.http.delete(URL + '/'+ id) as Observable<Movie>;
   }
 }
