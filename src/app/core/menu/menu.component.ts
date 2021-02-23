8d3864517df6fa6643a89fb923cf276880cd03c9
Dana Kimmel <dear.prudence34@gmail.com>
2021-02-23 17:40:50 -0500

    testing

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -7,18 +7,18 @@ import { MenuItem } from 'src/app/model/menu-item.class';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-  menuItems: MenuItem[]= [
-    new MenuItem("Movie","/movie-list","Movie List"),
-    new MenuItem("Actor","/actor-list","Actor List"),
-    new MenuItem("Credit","/credit-list","Credit List"),
-    new MenuItem("User","/user-list","User List"),
+  menuItems: MenuItem[] = [
+    new MenuItem("Movie", "/movie-list", "Movie List"),
+    new MenuItem("Actor", "/actor-list", "Actor List"),
+    new MenuItem("Credit", "/credit-list", "Credit List"),
+    new MenuItem("User", "/user-list", "User List"),
     new MenuItem("Login", "/user-login", "User Login")
   ];
 
   constructor() { }
 
   ngOnInit(): void {
-
+    //testing change for git
   }
 
 }
