c9aaf6fa24c6540487239c8518220af5e9e4ce55
Dana Kimmel <dear.prudence34@gmail.com>
2021-03-05 20:32:06 -0500

    user-detail.ts

--- src/app/feature/user/user-detail/user-detail.component.ts
+++ src/app/feature/user/user-detail/user-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/model/user.class';
+import { SystemService } from 'src/app/service/system.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  title: 'User Detail';
+  user: User = null;
+  userId: number = 0;
+    
+    constructor(
+      private userSvc: UserService,
+      private router: Router,
+      private route: ActivatedRoute,
+      private systemSvc: SystemService
+    ) {}
+  
+    ngOnInit(): void {
+      // get the id from the url
+      this.route.params.subscribe((parms) => {
+        this.userId = parms['id'];
+      });
+      // get user by id
+      this.userSvc.getById(this.userId).subscribe(
+        (resp) => {
+          this.user = resp as User;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
+    delete() {
+      // delete the user
+      this.userSvc.delete(this.user.id).subscribe(
+        (resp) => {
+          this.user = resp as User;
+          // forward to the user list component
+          this.router.navigateByUrl('/user-list');
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
   }
 
-}
+
